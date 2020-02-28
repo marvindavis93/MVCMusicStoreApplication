d286433feb9b6c269155c0fa2492e8da57892853
marvin davis <S01024543@acad.tri-c.edu>
2020-02-27 22:02:31 -0500

    Lab6

---
+++ MVCMusicStoreApplication/Models/Order.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+namespace MVCMusicStoreApplication.Models
+{
+    public class Order
+    {
+        public int OrderId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public string Username { get; set; }
+
+        [Required]
+        [StringLength(160, MinimumLength =3)]
+
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(160, MinimumLength =3)]
+        public string LastName { get; set; }
+
+
+
+
+        public string Adress {get; set;}
+    public string City { get; set; }
+    public string State { get; set; }
+    public string PostalCose { get; set; }
+    public string Country { get; set; }
+    public string Phone { get; set; }
+
+        [Required]
+        [RegularExpression(@"", ErrorMessage = "Please enter a vaild email" )]
+   public string Email { get; set; }
+
+        [Required]
+        [Range(typeof(decimal), "0.00 ", "49.99")]
+    public decimal Total { get; set; }
+    
+    
+    
+    }
+}
